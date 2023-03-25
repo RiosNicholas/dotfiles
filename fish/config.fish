55e2b1d2035260a65534b3a143531fac0e8aadf3
Nicholas <85625773+RiosNicholas@users.noreply.github.com>
2023-03-25 18:06:10 -0400

    Add files via upload

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,4 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
 
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-eval /Users/nicholasrios/opt/anaconda3/bin/conda "shell.fish" "hook" $argv | source
-# <<< conda initialize <<<
-
+end
