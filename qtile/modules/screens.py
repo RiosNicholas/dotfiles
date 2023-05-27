49c85ab2826747205bdbfba998b38167b2ec0573
Nicholas <85625773+RiosNicholas@users.noreply.github.com>
2023-05-26 23:13:38 -0400

    Update screens.py
    - Improved code formatting
    - Added/modified widgets

--- qtile/modules/screens.py
+++ qtile/modules/screens.py
@@ -8,26 +8,29 @@ screens = [
     Screen(
         top=bar.Bar(
             [   widget.Sep(padding=3, linewidth=0, background="#00000000"),
-                widget.Image(filename='~/.config/qtile/eos-c.png', margin=3, background="#2f343f", mouse_callbacks={'Button1': lambda: qtile.cmd_spawn("rofi -show combi")}),
-                widget.Sep(padding=4, linewidth=0, background="#2f343f"), 
-                widget.GroupBox(
-                                highlight_method = 'line',
-                                this_screen_border = catppuccin["blue"],
-                                this_current_screen_border = catppuccin["blue"],
-                                active = catppuccin["white"],
-                                inactive = catppuccin["green"],
-                                background = catppuccin["mantle"]),
+                widget.Image(filename='~/.config/qtile/eos-c.png', margin=3, background=catppuccin["mantle"], mouse_callbacks={'Button1': lambda: qtile.cmd_spawn("rofi -show combi")}),
+                widget.Sep(padding=4, linewidth=0, background=catppuccin["mantle"]), 
                 widget.TextBox(
-                       text = '',
-                       padding = 0,
-                       fontsize = 28,
-                       foreground = catppuccin["mauve"],
-                       background ="#00000000"),
-                widget.Prompt(),
-                widget.Spacer(
-                    length=3,
-                    background="#00000000"
+                    text = '',
+                    padding = 0,
+                    fontsize = 28,
+                    foreground = catppuccin["mauve"],
+                        ),
+                widget.GroupBox(
+                    highlight_method = 'block',
+                    this_screen_border = catppuccin["gray"],
+                    this_current_screen_border = catppuccin["mantle"],
+                    active = catppuccin["sky"],
+                    inactive = catppuccin["crust"],
+                    background = catppuccin["mauve"]
                     ),
+                widget.TextBox(
+                    text = '',
+                    padding = 0,
+                    fontsize = 28,
+                    foreground = catppuccin["mauve"]
+                    ),
+                widget.Prompt(),
                 widget.WindowName(foreground = catppuccin["sapphire"], fmt='{}'),
                 widget.Chord(
                     chords_colors={
@@ -40,42 +43,30 @@ screens = [
                     update_interval=1800,
                     distro="Arch_yay",
                     display_format="{updates} Updates",
-                    foreground=catppuccin["white"],
+                    foreground=catppuccin["pink"],
                     mouse_callbacks={
                         'Button1':
                         lambda: qtile.cmd_spawn(terminal + ' -e yay -Syu')
                     },
                     background=catppuccin["mantle"]),
-                widget.Systray(icon_size = 20),
+                widget.Systray(icon_size = 20), 
+                volume,  
                 widget.TextBox(
-                       text = '',
-                       padding = 0,
-                       fontsize = 28,
-                       foreground=catppuccin["mantle"]
-                       ), 
-                volume,
-                widget.TextBox(                                                                    
-                       text = '',
-                       padding = 0,
-                       fontsize = 28,
-                       foreground=catppuccin["mantle"]
-                       ),   
-                widget.TextBox(
-                       text = '',
-                       padding = 0,
-                       fontsize = 28,
-                       foreground=catppuccin["mantle"]
-                       ),    
+                    text = '',
+                    padding = 0,
+                    fontsize = 28,
+                    foreground=catppuccin["blue"]
+                    ),    
                 widget.Clock(format='󰥔 %Y-%m-%d %a %I:%M:%S %p',
-                             background=catppuccin["mantle"],
-                             foreground=catppuccin["green"]),
-                                                widget.TextBox(                                                
-                                                
-                       text = '',
-                       padding = 0,
-                       fontsize = 28,
-                       foreground = catppuccin["green"],
-                       ),   
+                    background=catppuccin["blue"],
+                    foreground=catppuccin["crust"]),
+                widget.TextBox(
+                    text = '',
+                    padding = 0,
+                    fontsize = 28,
+                    foreground = catppuccin["blue"],
+                    ),
+
                 widget.TextBox(
                     text='',
                     mouse_callbacks= {
@@ -83,6 +74,10 @@ screens = [
                         lambda: qtile.cmd_spawn(os.path.expanduser('~/.config/rofi/powermenu.sh'))
                     },
                     foreground=catppuccin["red"]),
+                 widget.Spacer(
+                    length=5,
+                    background=catppuccin["mantle"]
+                    ),
             ],
             30,  # height in px
             background=catppuccin["mantle"]  # background color
