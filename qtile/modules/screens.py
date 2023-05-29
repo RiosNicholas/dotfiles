6c97f60156070b085095d43a3ebc7c9b503e91e9
Nicholas <85625773+RiosNicholas@users.noreply.github.com>
2023-05-29 11:23:10 -0400

    Update screens.py

--- qtile/modules/screens.py
+++ qtile/modules/screens.py
@@ -3,17 +3,23 @@ from .widgets import *
 from libqtile.config import Screen
 from modules.keys import terminal
 import os
+import subprocess
+
+import subprocess
+subprocess.call(["xrandr", "--output", "DP-0", "--primary", "--rate", "144"])
+subprocess.call(["xrandr", "--output", "HDMI-0", "--left-of", "DP-0"])
+
 
 screens = [
     Screen(
         top=bar.Bar(
             [   widget.Sep(padding=3, linewidth=0, background="#00000000"),
-                widget.Image(
-                    filename='~/.config/qtile/eos-c.png', 
-                    margin=3, 
-                    background=catppuccin["mantle"], 
-                    mouse_callbacks={'Button1': lambda: qtile.cmd_spawn("rofi -show combi")}
-                    ),
+                # widget.Image(
+                #     filename='~/.config/qtile/eos-c.png', 
+                #     margin=3, 
+                #     background=catppuccin["mantle"], 
+                #     mouse_callbacks={'Button1': lambda: qtile.cmd_spawn("rofi -show combi")}
+                #     ),
                 widget.Sep(
                     padding=4, 
                     linewidth=0, 
@@ -23,7 +29,7 @@ screens = [
                     text = '',
                     padding = 0,
                     fontsize = 28,
-                    foreground = catppuccin["lavender"],
+                    foreground = catppuccin["mauve"],
                 ),
                 widget.GroupBox(
                     highlight_method = 'block',
@@ -36,14 +42,6 @@ screens = [
                     urgent_alert_method="block",
                     urgent_border=catppuccin["lavender"],
                     urgent_text=catppuccin["crust"],
-                    decorations=[
-                        RectDecoration(
-                            colour=catppuccin["lavender"],
-                            radius= 8,
-                            filled=True,
-                            # group=True
-                        )
-                    ],
                 ),
                 widget.TextBox(
                     text = '',
