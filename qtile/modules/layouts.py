0385c3deba8610b72b27a9f86425d3a836b95edd
Nicholas <85625773+RiosNicholas@users.noreply.github.com>
2023-05-29 11:24:18 -0400

    Update layouts.py

--- qtile/modules/layouts.py
+++ qtile/modules/layouts.py
@@ -1,9 +1,10 @@
 from libqtile import layout
 from libqtile.config import Match
+from .widgets import *
 
 layouts = [
-    layout.MonadTall(margin=8, border_focus='#5294e2',
-                     border_normal='#2c5380'),
+    layout.MonadTall(margin=8, border_focus=catppuccin["sky"],
+                     border_normal=catppuccin["mantle"]),
     #layout.Columns(border_focus_stack='#d75f5f'),
     layout.Max(),
     # Try more layouts by unleashing below layouts.
