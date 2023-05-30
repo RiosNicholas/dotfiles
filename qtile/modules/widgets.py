4d1a3dc075bbbb557f91618ae2bb2e6b7066b924
Nicholas <85625773+RiosNicholas@users.noreply.github.com>
2023-05-29 23:50:22 -0400

    Update widgets.py

--- qtile/modules/widgets.py
+++ qtile/modules/widgets.py
@@ -19,14 +19,17 @@ catppuccin = {
     "mantle": "#1e2030",
     "crust": "#181926",
     "lavender": "#b7bdf8",
-}
+    "yellow": "#eed49f",
+} 
+
 
 widget_defaults = dict(
     font='JetBrains Mono Nerd Font',
     fontsize=12,
-    padding=3
+    padding=10
 )
 extension_defaults = widget_defaults.copy()
+
 class MyVolume(widget.Volume):
     def _configure(self, qtile, bar):
         widget.Volume._configure(self, qtile, bar)
@@ -59,7 +62,8 @@ class MyVolume(widget.Volume):
 volume = MyVolume(
     fontsize=18,
     font='JetBrains Mono Nerd Font',
-    foreground=catppuccin["green"],
-    background=catppuccin["mantle"],
+    padding=10,
+    foreground=catppuccin["crust"],
+    background=catppuccin["green"],
     mouse_callbacks = {'Button1': lambda: qtile.cmd_spawn("pavucontrol")}
 )
