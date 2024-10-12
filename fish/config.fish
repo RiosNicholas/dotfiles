5901332650f954a09992cd15c0795c0d701ef9be
nicholas 🧙‍♂️ <85625773+RiosNicholas@users.noreply.github.com>
2024-10-12 17:10:47 -0400

    Add files via upload

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,5 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+     # Commands to run in interactive sessions can go here
 end
 
 set PATH $PATH ~/.config/emacs/bin
